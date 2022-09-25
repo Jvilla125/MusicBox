6497baf05c8b1bfd0605df8a563a80a31fb8729c
Jvilla125 <Jvilla125@gmail.com>
2022-09-25 13:32:09 -0700

    Update log in page to have a username and password input field

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,18 +2,31 @@ import React, { useState } from "react";
 import "./LoginPage.css";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 import userService from "../../utils/userService";
+import { Button, Checkbox, Form, Grid } from 'semantic-ui-react'
 
 export default function LoginPage(props) {
   return (
-    <>
-      <h1>Setup Login Page</h1>
-      <ul>
-        <li>Read the Login Model, You can change it to fit your needs</li>
-        <li>
-          Make sure you read the Login Controller, to know how it is setup to
-          find the user!
-        </li>
-      </ul>
-    </>
+      // <h1>Setup Login Page</h1>
+      <Grid
+      textAlign="center"
+      style={{ height: "100vh", width: "100vw" }}
+      verticalAlign="middle"
+    >
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <h1 as="h2">Please log in below</h1>
+      <Form>
+        
+      <Form.Field>
+        <label>Username</label>
+        <input />
+      </Form.Field>
+      <Form.Field>
+        <label>Password</label>
+        <input />
+      </Form.Field>
+    </Form>
+    
+    </Grid.Column>
+    </Grid>
   );
 }
