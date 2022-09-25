bc549dbd1e01e6f331a6ab191071af29804e6af5
Jvilla125 <Jvilla125@gmail.com>
2022-09-25 15:03:26 -0700

    Added handleSubmit function

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -6,7 +6,12 @@ import { useNavigate } from "react-router-dom";
 import { Button, Checkbox, Form, Grid, Segment } from 'semantic-ui-react'
 
 export default function SignUpPage(props) {
-  const [error, setError] = useState('')
+  const [error, setError] = useState({
+    message: "",
+    passwordError: false
+  })
+
+  // setting up the useState for user sign up form below
   const [state, setState] = useState({
     username: '',
     email: '',
@@ -14,12 +19,38 @@ export default function SignUpPage(props) {
     passwordConf: '',
   })
 
+  // setting up the useState for the photo upload below
+  const [selectedFile, setSelectedFile] = useState('')
+
   function handleChange(e) {
     setState({
       ...state,
       [e.target.name]: e.target.value
     })
   }
+  function handleImageInput(e){
+    console.log(e.target.files,"<-- this is the image upload")
+    setSelectedFile(e.target.files[0])
+  }
+
+  // function to handle form submission using 'formdata' 
+  // 
+  async function handleSubmit(e){
+    e.preventDefault()
+    const formData = new FormData()
+    formData.append('photo', selectedFile)
+    for (let value in state){
+      formData.append(value, state[value]);
+    }
+    
+    try {
+      await userService.signup(formData);
+    } catch(err){
+      setError(err.message)
+    }
+  }
+
+
 
   return (
     <Grid
@@ -35,31 +66,33 @@ export default function SignUpPage(props) {
               name="username"
               placeholder="username"
               value={state.username}
+              onChange={handleChange}
             />
             <Form.Input
               name="email"
               placeholder="email"
               value={state.email}
+              onChange={handleChange}
             />
             <Form.Input
               name="password"
               placeholder="password"
               value={state.password}
+              onChange={handleChange}
             />
             <Form.Input
               name="passwordConf"
               placeholder="password Confirmation"
               value={state.passwordConf}
+              onChange={handleChange}
             />
-            <Form.Field>
-              <Form.Input
-                type="file"
-                name="photo"
-                placeholder="upload image"
-                // onChange={handleFileInput}
-              />
-            </Form.Field>
-            <Button>
+            <Form.Input
+              type="file"
+              name="photo"
+              placeholder="upload image"
+              onChange={handleImageInput}
+            />
+            <Button type='submit' >
               Sign up
             </Button>
           </Segment>
