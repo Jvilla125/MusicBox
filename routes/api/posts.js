16ca8eacfb2e1818c1983e934338694d51a46ac4
Jvilla125 <Jvilla125@gmail.com>
2022-09-26 11:44:01 -0700

    Create a route for the posts api

---
+++ routes/api/posts.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const postsController = require('../../controllers/posts');
+const multer = require('multer');
+const upload = multer()
+
+router.post('/', upload.single('photo', postsController.create))
