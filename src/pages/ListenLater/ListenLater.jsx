71a44ebb634f685635f594cbc8270a836c6d1379
Jvilla125 <Jvilla125@gmail.com>
2022-09-29 13:34:48 -0700

    Update the 'ListenLater' page to include the PageHeader component'

--- src/pages/ListenLater/ListenLater.jsx
+++ src/pages/ListenLater/ListenLater.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import PageHeader from "../../components/Header/Header"
+import DisplayPosts from "../../components/DisplayPosts/DisplayPosts"
 
 export default function ListenLater(){
-    return(<h1>Listen to these songs later</h1>
+    return(
+        <>
+        <PageHeader/>
+        <h1>Listen to these songs later</h1>
+        </>
+
 )}
